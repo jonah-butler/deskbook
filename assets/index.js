7101e099f5ebbf1d1c6e86fc72faaba648c5fa6b
Jonah Butler <jonahbutler6@gmail.com>
2021-02-08 09:24:23 -0500

    updated modal listener to handle array of links

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,12 @@
 import { modalClickListener } from './scripts/modal.js';
 import { submitReference } from './scripts/async.js';
 
+let modalBtnArr = new Array();
 const referenceModalBtn = document.querySelector('.glyphicon-plus');
+modalBtnArr.push(referenceModalBtn);
+if(document.querySelector('#modalListener')){
+  const landingModalBtn = document.querySelector('#modalListener');
+  modalBtnArr.push(landingModalBtn);
+}
 
-modalClickListener(referenceModalBtn, submitReference);
+modalClickListener(modalBtnArr, submitReference);
