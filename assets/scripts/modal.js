961f965c4b3e2fe8085f52e9900f3613c697a98c
Jonah Butler <jonahbutler6@gmail.com>
2021-02-08 09:25:25 -0500

    cleaned up modal file - updated to accept array argument

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -1,22 +1,9 @@
-function loadModal(data = {}) {
-  const modal = document.querySelector('#Modal');
-  const modalContentCont = modal.querySelector('.modal-content-container');
-
-  modal.style.display = "block";
-  modal.onclick = function(e) {
-    if(e.target.className == 'modal' || e.target.className == 'close'){
-      modal.style.display = "none";
-      // modalContentCont.removeChild(document.querySelector('.video-wrapper'));
-    }
-  }
-    // submitReference();
-  }
-
-  // export { loadModal };
-
-function modalClickListener(button, callback) {
-  if(button){
-    button.
+function modalClickListener(buttonArr, callback) {
+  if(buttonArr){
+    Array.
+    from(buttonArr).
+    forEach((node) => {
+    node.
     addEventListener('click', function(e) {
       const modal = document.querySelector('#Modal');
       const modalContentCont = modal.querySelector('.modal-content-container');
@@ -25,11 +12,11 @@ function modalClickListener(button, callback) {
       modal.onclick = function(e) {
         if(e.target.className == 'modal' || e.target.className == 'close'){
           modal.style.display = "none";
-          // modalContentCont.removeChild(document.querySelector('.video-wrapper'));
         }
       }
         callback();
     })
+  })
   }
 }
 
