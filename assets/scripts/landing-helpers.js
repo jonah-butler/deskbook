e998090e37a8f5e201879dd32fb4ba5680afcd02
jonah-butler <jonahbutler6@gmail.com>
2021-04-14 16:34:24 -0400

    added helper script file for updated landing page, and index js file for landing - just handling some dom manipulation and event fetching through the library libcal api endpoint

---
+++ assets/scripts/landing-helpers.js
@@ -0,0 +1,55 @@
+async function getLibcalEvents() {
+  try{
+    let response = await fetch("/calendarClient");
+    response = await response.json();
+
+    let tokenResponse = await fetch('https://rvalibrary.libcal.com/1.1/oauth/token', {
+      method: 'POST',
+      body: JSON.stringify({
+        client_id: '570',
+        client_secret: response.clientSecret,
+        grant_type: 'client_credentials'
+      }),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    });
+    tokenResponse = await tokenResponse.json();
+
+    let events = await fetch(`https://rvalibrary.libcal.com/1.1/events?cal_id=14747&days=0`, {
+      headers: {
+        'Authorization': `Bearer ${tokenResponse.access_token}`
+      }
+    });
+    events = await events.json();
+    return events;
+  } catch(e){
+    console.log(e);
+  }
+}
+
+function buildEventCards(event, gradientName, gradient) {
+
+  const date = new Date();
+
+  let start = new Date(event.start);
+  start = new Intl.DateTimeFormat('en-us', {timeStyle: 'short'}).format(start);
+
+  let end = new Date(event.end);
+  end = new Intl.DateTimeFormat('en-us', {timeStyle: 'short'}).format(end);
+
+  return `<div class="event-panel fg1 ${gradientName}">
+    ${gradient}
+    <div class="event-panel-title">
+      <h5>${event.title}</h5>
+    </div>
+    <div class="event-panel-date">
+      ${date.toDateString()}
+    </div>
+    <div class="event-panel-time">
+      ${start} - ${end}
+    </div>
+  </div>`;
+}
+
+export { getLibcalEvents, buildEventCards };
