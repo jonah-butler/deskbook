8bcd20fdea74c5a2199cf46c7a2c7390b6ab554b
jonah-butler <jonahbutler6@gmail.com>
2021-04-21 15:38:57 -0400

    included today and yesterday quick search features

--- assets/user-reference.js
+++ assets/user-reference.js
@@ -5,7 +5,11 @@ import { createCanvasAndAppend } from './scripts/canvas-helpers.js';
 
 let branchBtn = document.querySelector('#branchDropdown');
 let branchMenu = document.querySelector('.dropdown-menu');
-let parentContainer = document.querySelector('.view-container')
+let parentContainer = document.querySelector('.view-container');
+let todayBtn = document.querySelector('#today');
+let tomorrowBtn = document.querySelector('#tomorrow');
+const inputStart = document.querySelector('#date-input-start');
+const inputEnd = document.querySelector('#date-input-end');
 
 let data;
 let chartDataObj;
@@ -43,3 +47,27 @@ document.querySelector('.icon-circle').addEventListener('click', async function(
     return;
   }
 })
+
+async function fetchToday() {
+  todayBtn.addEventListener('click', (e) => {
+    let today = new Date();
+    today = `${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()}`;
+    inputStart.value = today;
+    inputEnd.value = today;
+    document.querySelector('.icon-circle').click();
+  })
+}
+
+async function fetchTomorrow() {
+  tomorrowBtn.addEventListener('click', (e) => {
+    let yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    yesterday = `${yesterday.getMonth() + 1}/${yesterday.getDate()}/${yesterday.getFullYear()}`;
+    inputStart.value = yesterday;
+    inputEnd.value = yesterday;
+    document.querySelector('.icon-circle').click();
+  })
+}
+
+fetchToday();
+fetchTomorrow();
