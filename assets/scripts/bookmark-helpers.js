0e5765d5aff54f170b32e9817f3459ab2dcc0c53
jonah-butler <jonahbutler6@gmail.com>
2021-05-12 16:23:12 -0400

    new file for client helper methods for bookmark functionality

---
+++ assets/scripts/bookmark-helpers.js
@@ -0,0 +1,35 @@
+import { animateFlashBox } from './link-copy.js';
+
+function bookmarkClickListener() {
+  const bookmark = document.querySelector('.glyphicon-bookmark');
+  if(bookmark){
+    bookmark.addEventListener('click', async function(e) {
+      this.classList.toggle('checked');
+      const bookMarkData = gatherBookmarkData(this);
+      const response = await postBookmark(bookMarkData);
+      animateFlashBox(response.result);
+    })  
+  }
+}
+
+function gatherBookmarkData(bookmark) {
+  const id = bookmark.getAttribute('data-id');
+  const type = bookmark.getAttribute('data-type');
+  return {id, type};
+}
+
+async function postBookmark(data =  {}) {
+  let response = await fetch('/bookmark', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-type': 'application/json',
+    },
+  });
+  response = await response.json();
+  return response;
+}
+
+
+
+export { bookmarkClickListener };
