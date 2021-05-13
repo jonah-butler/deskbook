29870dfb3f91d2cf8459523cc0a9a02270ea738b
jonah-butler <jonahbutler6@gmail.com>
2021-05-13 13:05:52 -0400

    removed trailing space

--- routes/bookmark.js
+++ routes/bookmark.js
@@ -2,7 +2,6 @@ const BookMarkController = require('../controllers/BookMarkController.js');
 const helpers = require('../assets/helpers/helpers.js');
 
 module.exports = (app) => {
-
   app.get('/user/:id/bookmarks', helpers.isLoggedIn,
   BookMarkController.bookmarkIndex);
 
