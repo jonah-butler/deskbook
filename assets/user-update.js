8369d3a0459f1263b721c3e5661f88c5afb6f23a
jonah-butler <jonahbutler6@gmail.com>
2021-05-24 14:54:11 -0400

    index js file for user update page

---
+++ assets/user-update.js
@@ -0,0 +1,6 @@
+import { toggleSubLocation } from './scripts/user-register-helpers.js';
+
+toggleSubLocation(
+  document.querySelector('#branchLocationRegister'),
+  document.querySelector('#mainSubContainerRegister'),
+)
