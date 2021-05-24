50ee59a37ef05fbf8b86be24e7ae6d47decd26e1
jonah-butler <jonahbutler6@gmail.com>
2021-05-24 17:23:41 -0400

    added middleware to route

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,6 @@ const HomeController = require('../controllers/HomeController.js');
 const helpers = require('../assets/helpers/helpers.js');
 
 module.exports = (app) => {
-  app.get('/', helpers.isLoggedIn,
+  app.get('/', helpers.isLoggedIn, helpers.shouldUserUpdateLibraryLocation,
   HomeController.index);
 }
