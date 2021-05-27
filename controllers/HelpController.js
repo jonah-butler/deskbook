65d1a0651a73fbbcfe791d4f05b000427a83f11f
jonah-butler <jonahbutler6@gmail.com>
2021-05-27 12:42:13 -0400

    added help controller to handle user submissions for guide requests

---
+++ controllers/HelpController.js
@@ -0,0 +1,46 @@
+const emoji = require('node-emoji');
+const sgMail = require('@sendgrid/mail');
+
+require('dotenv').config()
+
+module.exports = {
+  async helpIndex(req, res) {
+    res.render('help', {
+      message: req.flash('message'),
+      emoji: emoji.get('thinking_face'),
+    });
+  },
+  async helpPost(req, res) {
+    try{
+      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+      const msg = {
+        to: 'jonah.butler@richmondgov.com',
+        from: 'rvalearns@gmail.com',
+        subject: 'Deskbook Guide Request',
+        text: `New Deskbook guide request from ${req.body.post.submitter}.
+
+        Respond to ${req.body.post.submitter} through ${req.body.post.email}.
+
+        Guide Being Requested:
+        _______________________
+        ${req.body.post.guide}
+        `
+      };
+
+      sgMail
+        .send(msg)
+        .then(() => {
+          req.flash('message', 'Your email was successfully sent!' );
+          res.redirect('/help');
+        })
+        .catch((error) => {
+          req.flash('message', 'an unexpected error occurred. contact the administrator');
+          res.redirect('/help');
+        })
+      } catch(err) {
+        req.flash('message', 'weird. something strange happened on the server.');
+        res.redirect('/help');
+      }
+  },
+}
