7459cc1d7bb500e3c104bc84e2c47c6ab59b8219
jonah-butler <jonahbutler6@gmail.com>
2021-06-02 11:50:03 -0400

    help route now handles patron/staff string when sending email, and added previous route logic to handle where users get forwarded after submitting a guide request

--- controllers/HelpController.js
+++ controllers/HelpController.js
@@ -5,14 +5,26 @@ require('dotenv').config()
 
 module.exports = {
   async helpIndex(req, res) {
+    let previousRoute = req.headers.referer;
+    let isPublic;
+    if(previousRoute.split(' ')[0].indexOf('public') != -1){
+      isPublic = true;
+    } else {
+      isPublic = false;
+    }
     res.render('help', {
       message: req.flash('message'),
       emoji: emoji.get('thinking_face'),
       user: req.user,
+      isPublic: isPublic,
+      referer: previousRoute,
+      lastRoute: req.flash('lastRoute'),
     });
   },
   async helpPost(req, res) {
     try{
+      const lastRoute = req.body.post.referer;
+
       sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
       const msg = {
@@ -20,6 +32,8 @@ module.exports = {
         from: 'rvalearns@gmail.com',
         subject: 'Deskbook Guide Request',
         text: `New Deskbook guide request from ${req.body.post.submitter}.
+
+        Location in ${req.body.post.location}
 
         Respond to ${req.body.post.submitter} through ${req.body.post.email}.
 
@@ -33,14 +47,17 @@ module.exports = {
         .send(msg)
         .then(() => {
           req.flash('message', 'Your email was successfully sent!' );
+          req.flash('lastRoute', lastRoute);
           res.redirect('/help');
         })
         .catch((error) => {
           req.flash('message', 'an unexpected error occurred. contact the administrator');
+          req.flash('lastRoute', lastRoute);
           res.redirect('/help');
         })
       } catch(err) {
         req.flash('message', 'weird. something strange happened on the server.');
+        req.flash('lastRoute', lastRoute);
         res.redirect('/help');
       }
   },
