e06cfa1c6f6f969aebcacd544654fa8ef2fec67d
jonah-butler <jonahbutler6@gmail.com>
2021-06-25 15:36:59 -0400

    updated query selector param to new side menu id

--- assets/scripts/menu-collapse/index.js
+++ assets/scripts/menu-collapse/index.js
@@ -1,6 +1,6 @@
 import {toggleSideMenu} from './menu-collapse.js';
 
-const sideMenu = document.querySelector('#sideMenu');
+const sideMenu = document.querySelector('#sideMenuNav');
 const hamburger = document.querySelector('#sideMenuBtn');
 
 toggleSideMenu(sideMenu, hamburger);
