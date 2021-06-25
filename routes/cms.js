47cd2f6655031a10b1a5d0741719c71c3852e388
jonah-butler <jonahbutler6@gmail.com>
2021-06-25 13:29:26 -0400

    initial route commit for cms

---
+++ routes/cms.js
@@ -0,0 +1,20 @@
+const CMSController = require('../controllers/CMSController.js');
+const helpers = require('../assets/helpers/helpers.js');
+
+module.exports = (app) => {
+
+  app.get('/media/:prefix?', helpers.isLoggedIn,
+  CMSController.getBucket);
+
+  app.post('/media/signed-url', helpers.isLoggedIn,
+  CMSController.getSignedURL);
+
+  app.post('/media/new-folder', helpers.isLoggedIn,
+  CMSController.newFolder);
+
+  app.post('/media/upload', helpers.isLoggedIn,
+  CMSController.upload);
+
+  app.post('/media/delete', helpers.isLoggedIn,
+  CMSController.delete);
+}
