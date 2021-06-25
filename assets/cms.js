54ed8d818acde2dda600cabf342ac9289a69588b
jonah-butler <jonahbutler6@gmail.com>
2021-06-25 13:27:04 -0400

    core js initializer for cms views

---
+++ assets/cms.js
@@ -0,0 +1,23 @@
+import {imageThumbnailListener, newFolderListener, submitFolder, uploadImageListener, fileUploadListener} from './scripts/cms-helpers.js';
+
+const thumbnails = document.querySelectorAll('.cms-thumbnail');
+const sideMenu = document.querySelector('#sideMenu');
+const sideMenuForm = document.querySelector('#sideMenuForm');
+const backdrop = document.querySelector('.backdrop');
+const sideMenuImg = document.querySelector('#sideMenuImg');
+const downloadBtn = document.querySelector('#download');
+const newFolderBtn = document.querySelector('#newFolder');
+const submitFolderBtn = document.querySelector('#submitFolder');
+const newFolderInput = document.querySelector('#folderName');
+const urlInput = document.querySelector('#imageUrl');
+const copyLink = document.querySelector('#copyLink');
+const newImgBtn = document.querySelector('#uploadImage');
+const sideMenuImgForm = document.querySelector('#sideMenuUpload');
+const fileInput = document.querySelector('#imageUpload');
+const deleteBtn = document.querySelector('#delete');
+
+imageThumbnailListener(thumbnails, sideMenu, backdrop, sideMenuImg, downloadBtn, urlInput, copyLink, deleteBtn);
+newFolderListener(newFolderBtn, sideMenuForm, backdrop);
+uploadImageListener(newImgBtn, sideMenuImgForm, backdrop);
+submitFolder(submitFolderBtn, newFolderInput);
+fileUploadListener(fileInput);
