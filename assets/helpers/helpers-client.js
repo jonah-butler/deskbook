2219ee74815a7ed591aa06edcda3057d7f827e7c
jonah-butler <jonahbutler6@gmail.com>
2021-06-28 13:01:55 -0400

    added a button toggle method for disabling a given button inside a click event listener, a method for building site loader and a method for removing the loader

--- assets/helpers/helpers-client.js
+++ assets/helpers/helpers-client.js
@@ -6,4 +6,22 @@ function formatDatesForDatePickers(date) {
   }
 }
 
-export default { formatDatesForDatePickers };
+function toggleButtonDisabled(btn) {
+  return btn.disabled = !btn.disabled;
+}
+
+function buildLoader(parentContainer) {
+  const loader = `<div class="loader">
+  					<div class="loader-inner">
+  						<div></div><div></div><div></div>
+  					</div>
+  				</div>`;
+  parentContainer.insertAdjacentHTML('afterbegin', loader);
+  return parentContainer.firstChild;
+}
+
+function removeLoader(loader) {
+  loader.remove();
+}
+
+export { formatDatesForDatePickers, toggleButtonDisabled, buildLoader, removeLoader };
