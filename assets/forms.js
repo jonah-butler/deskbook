b3af743932c487430a1e7457ba0e5eb6dffea8c5
jonah-butler <jonahbutler6@gmail.com>
2021-07-02 11:24:38 -0400

    future file for client side js relating to form validation

---
+++ assets/forms.js
@@ -0,0 +1,8 @@
+import {toggleButtonDisabled, removeLoader, buildLoader} from './helpers/helpers-client.js';
+
+// const formSubmitBtn = document.querySelector('.formSubmit');
+//
+// formSubmitBtn.addEventListener('click', function(e) {
+//   toggleButtonDisabled(this);
+//   // buildLoader(document.querySelector('.submitBtnContainer'));
+// })
