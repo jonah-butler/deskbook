ab9c4ba6d0fb904f78243250b8128d774c09d8f2
jonah-butler <jonahbutler6@gmail.com>
2021-07-21 15:27:42 -0400

    added new chart definitions, lineChart

--- assets/landing.js
+++ assets/landing.js
@@ -3,11 +3,12 @@ import { getReferenceData, fetchAndBuild } from './scripts/landing-helpers.js';
 
 const eventContainer = document.querySelector('.event-card-container');
 const loader = document.querySelector('.loader');
+const lineChart = document.querySelector('.hour-chart');
 const chart1 = document.querySelector('.chart1');
 const chart2 = document.querySelector('.chart2');
 
 fetchAndBuild(eventContainer, loader);
-getReferenceData(chart1, chart2);
+getReferenceData(chart1, chart2, lineChart);
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();
 });
